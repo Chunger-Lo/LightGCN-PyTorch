420f7acdb812cd673b5dd45e25438fef2bb43502
Chunger-Lo <chunger0828@gmail.com>
2022-03-07 14:49:32 +0800

    [REFACTOR] Minor revisement for parameters and add eda code.

--- code/parse.py
+++ code/parse.py
@@ -16,14 +16,14 @@ def parse_args():
                         help="the embedding size of lightGCN")
     parser.add_argument('--layer', type=int,default=3,
                         help="the layer num of lightGCN")
-    parser.add_argument('--lr', type=float,default=0.01,
+    parser.add_argument('--lr', type=float,default=0.001,
                         help="the learning rate")
     parser.add_argument('--decay', type=float,default=1e-4,
                         help="the weight decay for l2 normalizaton")
     parser.add_argument('--dropout', type=int,default=0,
                         help="using the dropout or not")
     parser.add_argument('--keepprob', type=float,default=0.6,
-                        help="the batch size for bpr loss training procedure")
+                        help="dropout prob")
     parser.add_argument('--a_fold', type=int,default=100,
                         help="the fold num used to split large adj matrix, like gowalla")
     parser.add_argument('--testbatch', type=int,default=512,
